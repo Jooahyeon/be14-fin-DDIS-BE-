44bd461f0b5fdb7cee5f2232ec4e8fb0c95052c0
LAPTOP-4PKBB9GJ\Playdata <tommy8969@naver.com>
2025-06-05 20:21:45 +0900

    refactor: responseEntity 적용

--- DDIS_HR/src/main/java/com/ddis/ddis_hr/attendance/query/controller/AttendanceQueryController.java
+++ DDIS_HR/src/main/java/com/ddis/ddis_hr/attendance/query/controller/AttendanceQueryController.java
@@ -5,6 +5,7 @@ import com.ddis.ddis_hr.attendance.query.dto.TeamCalendarQueryDTO;
 import com.ddis.ddis_hr.attendance.query.service.AttendanceQueryService;
 import com.ddis.ddis_hr.member.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,15 +18,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AttendanceQueryController {
 
-    private final AttendanceQueryService personalCalendarQueryService;
+    private final AttendanceQueryService attendanceQueryService;
 
     @GetMapping("/calendar/personal")
-    public List<PersonalCalendarQueryDTO> getMyCalendar(@AuthenticationPrincipal CustomUserDetails user) {
-        return personalCalendarQueryService.getMyCalendar(user.getEmployeeId());
+    public ResponseEntity<List<PersonalCalendarQueryDTO>> getMyCalendar(@AuthenticationPrincipal CustomUserDetails user) {
+        List<PersonalCalendarQueryDTO> result = attendanceQueryService.getMyCalendar(user.getEmployeeId());
+        return ResponseEntity.ok(result); // HTTP 200 OK
     }
 
     @GetMapping("/calendar/team")
-    public List<TeamCalendarQueryDTO> getTeamCalendar(@AuthenticationPrincipal CustomUserDetails user) {
-        return personalCalendarQueryService.getTeamCalendar(user.getEmployeeId());
+    public ResponseEntity<List<TeamCalendarQueryDTO>> getTeamCalendar(@AuthenticationPrincipal CustomUserDetails user) {
+        List<TeamCalendarQueryDTO> result = attendanceQueryService.getTeamCalendar(user.getEmployeeId());
+        return ResponseEntity.ok(result); // HTTP 200 OK
     }
 }

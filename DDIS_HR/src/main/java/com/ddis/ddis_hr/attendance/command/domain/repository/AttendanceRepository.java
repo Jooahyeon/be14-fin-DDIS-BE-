f48d126f2649b77bcc4b24e6bbe9b4ff8a2b9ca5
LAPTOP-4PKBB9GJ\Playdata <tommy8969@naver.com>
2025-06-09 17:32:03 +0900

    feat: 근무 repository 구현

---
+++ DDIS_HR/src/main/java/com/ddis/ddis_hr/attendance/command/domain/repository/AttendanceRepository.java
@@ -0,0 +1,15 @@
+package com.ddis.ddis_hr.attendance.command.domain.repository;
+
+import com.ddis.ddis_hr.attendance.command.domain.aggregate.Attendance;
+import com.ddis.ddis_hr.member.command.domain.aggregate.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
+
+    Optional<Attendance> findByEmployeeAndWorkDate(Employee employee, LocalDate workDate);
+
+    boolean existsByEmployeeAndWorkDate(Employee employee, LocalDate workDate);
+}

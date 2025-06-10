fc468320da348de99b2cab4c4d2818337ff3e513
LAPTOP-4PKBB9GJ\Playdata <tommy8969@naver.com>
2025-06-10 13:55:30 +0900

    feat: 개인 일정 등록 controller 구현

--- DDIS_HR/src/main/java/com/ddis/ddis_hr/attendance/command/application/controller/AttendanceCommandController.java
+++ DDIS_HR/src/main/java/com/ddis/ddis_hr/attendance/command/application/controller/AttendanceCommandController.java
@@ -1,5 +1,6 @@
 package com.ddis.ddis_hr.attendance.command.application.controller;
 
+import com.ddis.ddis_hr.attendance.command.application.dto.PersonalScheduleRequestDTO;
 import com.ddis.ddis_hr.attendance.command.application.service.AttendanceCommandService;
 import com.ddis.ddis_hr.member.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
@@ -12,18 +13,26 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/attendance")
 public class AttendanceCommandController {
 
-    private final AttendanceCommandService checkInService;
+    private final AttendanceCommandService attendanceCommandService;
 
     @PostMapping("/check-in")
     public ResponseEntity<String> checkIn(@AuthenticationPrincipal CustomUserDetails user) {
-        checkInService.checkIn(user.getEmployeeId());
+        attendanceCommandService.checkIn(user.getEmployeeId());
         return ResponseEntity.ok("출근이 정상적으로 등록되었습니다.");
     }
 
     @PutMapping("/check-out")
     public ResponseEntity<String> checkOut(@AuthenticationPrincipal CustomUserDetails user) {
-        checkInService.checkOut(user.getEmployeeId());
+        attendanceCommandService.checkOut(user.getEmployeeId());
         return ResponseEntity.ok("퇴근이 정상적으로 등록되었습니다.");
+    }
+
+    @PostMapping("/schedule/personal")
+    public ResponseEntity<Void> addPersonalSchedule(@RequestBody PersonalScheduleRequestDTO dto,
+                                                    @AuthenticationPrincipal CustomUserDetails user) {
+        Long employeeId = user.getEmployeeId();
+        attendanceCommandService.personalScheduleRegister(dto, employeeId);
+        return ResponseEntity.ok().build();
     }
 
 }

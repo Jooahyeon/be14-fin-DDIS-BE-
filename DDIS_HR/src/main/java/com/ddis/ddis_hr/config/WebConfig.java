554da74f4e0213eebe8cd6b4c89446facc106cb5
RKDLDE <kangcanddy@gmail.com>
2025-06-10 16:08:41 +0900

    [chore] patch, option 추가

--- DDIS_HR/src/main/java/com/ddis/ddis_hr/config/WebConfig.java
+++ DDIS_HR/src/main/java/com/ddis/ddis_hr/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")                                // 全 경로
                 .allowedOrigins("http://localhost:8080")         // Vue dev 서버
-                .allowedMethods("GET","POST","PUT","DELETE")     // 허용 HTTP 메서드
+                .allowedMethods("GET","POST","PUT","DELETE", "PATCH", "OPTIONS")     // 허용 HTTP 메서드
                 .allowedHeaders("*")                             // 모든 헤더 허용
                 .allowCredentials(true);                         // 쿠키/인증 헤더 허용
     }

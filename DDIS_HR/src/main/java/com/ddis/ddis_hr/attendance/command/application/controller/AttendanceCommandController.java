cab66f731b2032daf50c41d7e36bf53c68c93bc7
LAPTOP-4PKBB9GJ\Playdata <tommy8969@naver.com>
2025-06-10 10:20:15 +0900

    feat: 출근 controller 구현

---
+++ DDIS_HR/src/main/java/com/ddis/ddis_hr/attendance/command/application/controller/AttendanceCommandController.java
@@ -0,0 +1,23 @@
+package com.ddis.ddis_hr.attendance.command.application.controller;
+
+import com.ddis.ddis_hr.attendance.command.application.service.AttendanceCommandService;
+import com.ddis.ddis_hr.member.security.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/attendance")
+public class AttendanceCommandController {
+
+    private final AttendanceCommandService checkInService;
+
+    @PostMapping("/check-in")
+    public ResponseEntity<String> checkIn(@AuthenticationPrincipal CustomUserDetails user) {
+        checkInService.checkIn(user.getEmployeeId());
+        return ResponseEntity.ok("출근이 정상적으로 등록되었습니다.");
+    }
+
+}

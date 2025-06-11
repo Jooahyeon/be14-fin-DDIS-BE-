deb45208f08d7d80950e889aba948a528ea6281b
rrangeess <zlions2127@gmail.com>
2025-06-11 17:43:55 +0900

    [FEAT] 사원 등록 API 구현

--- DDIS_HR/src/main/java/com/ddis/ddis_hr/member/command/application/dto/EmployeeDTO.java
+++ DDIS_HR/src/main/java/com/ddis/ddis_hr/member/command/application/dto/EmployeeDTO.java
@@ -2,6 +2,7 @@ package com.ddis.ddis_hr.member.command.application.dto;
 
 import lombok.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
@@ -11,28 +12,29 @@ import java.time.LocalDateTime;
 @ToString
 public class EmployeeDTO {
 
-    private Long employeeId;
+    private Integer employeeId;
     private String employeeName;
     private String employeePwd;
-    private String employeeProfile;
+    private String employeePhotoName;
+    private String employeePhotoUrl;
     private String employeeNation;
     private String employeeGender;
-    private LocalDateTime employeeBirth;
+    private LocalDate employeeBirth;
     private String employeeResident;
     private String employeeContact;
     private String employeeEmail;
     private String employeeAddress;
-    private LocalDateTime employmentDate;
-    private LocalDateTime returementDate;
+    private LocalDate employmentDate;
+    private LocalDate retirementDate;
     private Boolean workType;
     private String bankName;
     private String bankDepositor;
     private String bankAccount;
     private Boolean isDisorder;
-    private String isMilitary;
+    private String militaryType;
     private Boolean isMarriage;
-    private LocalDateTime marriageDate;
-    private Integer familiyCount;
+    private LocalDate marriageDate;
+    private Integer familyCount;
     private Integer careerYearCount;
     private String previousCompany;
     private String finalAcademic;
@@ -40,7 +42,6 @@ public class EmployeeDTO {
     private String employeeDept;
     private Integer graduationYear;
     private Boolean isFourInsurances;
-
     private Long positionId;
     private Long rankId;
     private Long jobId;

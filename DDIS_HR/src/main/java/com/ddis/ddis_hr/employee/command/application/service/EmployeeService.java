11e4e650de0317ba855df36b79ab190e69101c31
rrangeess <zlions2127@gmail.com>
2025-06-11 21:10:23 +0900

    [FEAT] 사원 정보 수정

--- DDIS_HR/src/main/java/com/ddis/ddis_hr/employee/command/application/service/EmployeeService.java
+++ DDIS_HR/src/main/java/com/ddis/ddis_hr/employee/command/application/service/EmployeeService.java
@@ -1,9 +1,15 @@
 package com.ddis.ddis_hr.employee.command.application.service;
 
 import com.ddis.ddis_hr.employee.command.application.dto.EmployeeEnrollDTO;
+import com.ddis.ddis_hr.employee.command.application.dto.EmployeeHrUpdateDTO;
+import com.ddis.ddis_hr.employee.command.application.dto.EmployeeUpdateDTO;
 
 
 public interface EmployeeService {
 
-    Long enrollEmployee(EmployeeEnrollDTO dto); // 사원 등록 비즈니스 로직 정의
+    Long enrollEmployee(EmployeeEnrollDTO dto);                             // 사원 등록
+
+    void updateEmployee(Long employeeId, EmployeeUpdateDTO dto);            // 사원 정보 수정 (일반 사원)
+
+//    void hrUpdateEmployee(Long employeeId, EmployeeHrUpdateDTO dto);     // 사원 정보 수정 (인사팀)
 }

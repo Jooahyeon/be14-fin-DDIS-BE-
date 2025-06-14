42d69652e204d900d4655663a51de07384c56b63
Jooahyeon <dkgus0496@naver.com>
2025-06-15 01:11:19 +0900

    [FEAT] 전자결재 결재함 구현 api

--- DDIS_HR/src/main/java/com/ddis/ddis_hr/eapproval/query/controller/ApprovalDocumentQueryController.java
+++ DDIS_HR/src/main/java/com/ddis/ddis_hr/eapproval/query/controller/ApprovalDocumentQueryController.java
@@ -1,5 +1,5 @@
 package com.ddis.ddis_hr.eapproval.query.controller;
-
+// 결재함 (결재 - 진행 - 완료 분기처리)
 
 import com.ddis.ddis_hr.eapproval.query.dto.DocumentDTO;
 import com.ddis.ddis_hr.eapproval.query.service.ApprovalDocumentQueryService;
@@ -9,8 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -20,12 +22,42 @@ public class ApprovalDocumentQueryController {
 
     private final ApprovalDocumentQueryService approvalDocumentQueryService;
 
+//    @GetMapping
+//    public ResponseEntity<List<DocumentDTO>> getPendingApprovals(@AuthenticationPrincipal CustomUserDetails user) {
+//        // Assuming principal.getName() returns employeeId as string
+//        Long employeeId = user.getEmployeeId();
+//        List<DocumentDTO> documents = approvalDocumentQueryService.getPendingApprovals(employeeId);
+//        return ResponseEntity.ok(documents);
+//    }
+
+
     @GetMapping
-    public ResponseEntity<List<DocumentDTO>> getPendingApprovals(@AuthenticationPrincipal CustomUserDetails user) {
-        // Assuming principal.getName() returns employeeId as string
+    public ResponseEntity<List<DocumentDTO>> getApprovals(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @RequestParam(name = "tab", defaultValue = "결재") String tab
+    ) {
         Long employeeId = user.getEmployeeId();
-        List<DocumentDTO> documents = approvalDocumentQueryService.getPendingApprovals(employeeId);
+        List<DocumentDTO> documents;
+
+        switch (tab) {
+            case "결재":
+                documents = approvalDocumentQueryService.getPendingApprovals(employeeId);
+                break;
+            case "진행":
+                documents = approvalDocumentQueryService.getInProgressApprovals(employeeId);
+                break;
+            case "완료":
+                documents = approvalDocumentQueryService.getCompletedApprovals(employeeId);
+                break;
+            case "전체":
+                documents = approvalDocumentQueryService.getAllApprovals(employeeId);
+                break;
+            default:
+                documents = Collections.emptyList();
+        }
+
         return ResponseEntity.ok(documents);
     }
+
 }
 
--- DDIS_HR/src/main/java/com/ddis/ddis_hr/eapproval/query/mapper/DraftDocumentMapper.java
+++ DDIS_HR/src/main/java/com/ddis/ddis_hr/eapproval/query/mapper/DraftDocumentMapper.java
@@ -1,6 +1,5 @@
 package com.ddis.ddis_hr.eapproval.query.mapper;
 
-import com.ddis.ddis_hr.eapproval.query.dto.DocumentDTO;
 import com.ddis.ddis_hr.eapproval.query.dto.DraftDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;

cfa4d872547bd2fec1d3d0d86b9d5025896cabee
1214alex <jerry20011214@gmail.com>
2025-06-27 14:12:39 +0900

    no message

--- DDIS_HR/src/main/java/com/ddis/ddis_hr/member/security/WebSecurity.java
+++ DDIS_HR/src/main/java/com/ddis/ddis_hr/member/security/WebSecurity.java
@@ -59,6 +59,8 @@ public class WebSecurity {
                                 .requestMatchers(new AntPathRequestMatcher("/users/**", "POST")).permitAll()
                                 .requestMatchers(new AntPathRequestMatcher("/members/**", "GET")).permitAll()
                                 .requestMatchers(new AntPathRequestMatcher("/email-verification/**")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/s3/**")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/api/password-reset/**")).permitAll()
 
                                 .anyRequest().authenticated()
                 )

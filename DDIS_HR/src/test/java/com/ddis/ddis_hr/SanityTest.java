0353e94478dec0e9f94f2e5871c12c5cfb7e3ae9
Jooahyeon <dkgus0496@naver.com>
2025-08-25 23:23:20 +0900

    test : 로컬 빌드 테스트코드

---
+++ DDIS_HR/src/test/java/com/ddis/ddis_hr/SanityTest.java
@@ -0,0 +1,12 @@
+package com.ddis.ddis_hr;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SanityTest {
+
+    @Test
+    void it_works() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+}
